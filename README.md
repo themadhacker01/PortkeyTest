19bab2c73f9dba01c9244601f350d406bb2d0ab2
themadhacker01 <ameyshitole152@gmail.com>
2024-03-31 13:01:01 +0530

    Used portkey gateway to prompt gemini

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Portkey Test
 
 ## Docs
+[Integrating Portkey SDK for Google Gemini](https://docs.portkey.ai/docs/welcome/integration-guides/gemini)
+
+[Portkey SDK client parameters and descriptions](https://docs.portkey.ai/docs/api-reference/portkey-sdk-client#parameters)
+
+[Obtaining your Portkey account API key](https://docs.portkey.ai/docs/api-reference/authentication#obtaining-your-api-key)
--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import yaml
 from portkey_ai import Portkey
 
+with open('assets/credentials.yml') as cred_file:
+    creds = yaml.safe_load(cred_file)
+
+PORTKEY_API_KEY = creds['portkey_api_key']
+PORTKEY_VIRTUAL_KEY = creds['portkey_virtual_key']
+
+pk_gateway = Portkey(
+    api_key = PORTKEY_API_KEY,
+    virtual_key = PORTKEY_VIRTUAL_KEY
+)
+
+response = pk_gateway.chat.completions.create(
+    messages = [
+        {
+            'role': 'user',
+            'content': 'Strictly refer to https://portkey.ai/ to tell me concisely what portkey does'
+        }
+    ],
+    model = 'gemini-pro'
+)
+
+print(response.choices[0].message.content)
